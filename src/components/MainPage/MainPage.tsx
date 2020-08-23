28c49bb51181fcce3e3d36d38d129637af415fb7
Abdulrahman Khankan <abdulrahman.khankan@gmail.com>
2020-08-23 17:49:19 +0200

    implement call and communication logic

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -6,14 +6,27 @@ import { ParticipantSelfStatus, ParticipantCallStatus } from '../shared.model';
 const PARTICIPANTS_COUNT = 4;
 
 interface IParticipant {
-  status: ParticipantSelfStatus;
+  selfStatus: ParticipantSelfStatus;
   callStatus?: ParticipantCallStatus;
+  secondParticipantId: string;
+  incomingMessage?: string;
+}
+
+interface IParticipants {
+  [id: string]: IParticipant;
 }
 
 interface IConnectionManager {
-  participants: {
-    [id: string]: IParticipant;
-  };
+  participants: IParticipants;
+  makeCall: (from: string, to: string) => void;
+  acceptCall: any;
+  rejectCall: any;
+  send: any;
+  endCall: any;
+}
+
+interface IState {
+  manager: IConnectionManager;
 }
 
 const ConnectionManagerContext = React.createContext({} as IConnectionManager);
@@ -21,26 +34,135 @@ ConnectionManagerContext.displayName = 'ConnectionManager';
 
 export const ConnectionManager = ConnectionManagerContext.Consumer;
 
-export class MainPage extends React.Component {
-  state = {
-    manager: {
-      participants: this.initializeParticipants(),
-    },
-  };
+export class MainPage extends React.Component<any, IState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      manager: {
+        participants: this.initializeParticipants(),
+        makeCall: this.makeCall,
+        acceptCall: this.acceptCall,
+        rejectCall: this.rejectCall,
+        send: this.send,
+        endCall: this.endCall,
+      },
+    };
+  }
 
-  initializeParticipants() {
+  initializeParticipants(): IParticipants {
     const participants: { [id: string]: IParticipant } = {};
 
     for (let i = 0; i < PARTICIPANTS_COUNT; i++) {
       const participantId = getRandomId();
 
       participants[participantId] = {
-        status: ParticipantSelfStatus.Idle,
+        selfStatus: ParticipantSelfStatus.Idle,
+        secondParticipantId: '',
       };
     }
 
     return participants;
   }
+
+  updateParticipantSelfStatus(participantId: string, newStatus: ParticipantSelfStatus): void {
+    const newState = { ...this.state };
+    newState.manager.participants[participantId].selfStatus = newStatus;
+
+    this.setState(newState);
+  }
+
+  updateParticipantCallStatus(participantId: string, newStatus: ParticipantCallStatus): void {
+    const newState = { ...this.state };
+    newState.manager.participants[participantId].callStatus = newStatus;
+
+    this.setState(newState);
+  }
+
+  makeCall = (sourceId: string, targetId: string): void => {
+    const target = this.state.manager.participants[targetId];
+
+    if (!target) {
+      return this.updateParticipantCallStatus(sourceId, ParticipantCallStatus.RemoteUnknown);
+    }
+
+    const destinationStatus = target.selfStatus;
+    if (destinationStatus !== ParticipantSelfStatus.Idle) {
+      this.updateParticipantCallStatus(sourceId, ParticipantCallStatus.RemoteBusy);
+    } else {
+      this.placeCall(sourceId, targetId);
+    }
+  };
+
+  private placeCall(sourceId: string, targetId: string): void {
+    const newState = { ...this.state };
+
+    newState.manager.participants[sourceId].selfStatus = ParticipantSelfStatus.PlacingCall;
+    newState.manager.participants[targetId].selfStatus = ParticipantSelfStatus.Ringing;
+
+    newState.manager.participants[sourceId].callStatus = ParticipantCallStatus.RemoteRinging;
+
+    newState.manager.participants[sourceId].secondParticipantId = targetId;
+    newState.manager.participants[targetId].secondParticipantId = sourceId;
+
+    this.setState(newState);
+  }
+
+  acceptCall = (participantId: string): void => {
+    const newState = { ...this.state };
+    const { secondParticipantId } = this.state.manager.participants[participantId];
+
+    newState.manager.participants[participantId].selfStatus = ParticipantSelfStatus.Talking;
+    newState.manager.participants[secondParticipantId].selfStatus = ParticipantSelfStatus.Talking;
+
+    newState.manager.participants[participantId].callStatus = undefined;
+    newState.manager.participants[secondParticipantId].callStatus = undefined;
+
+    this.setState(newState);
+  };
+
+  rejectCall = (participantId: string): void => {
+    const newState = { ...this.state };
+    const { secondParticipantId } = this.state.manager.participants[participantId];
+
+    newState.manager.participants[participantId].selfStatus = ParticipantSelfStatus.Idle;
+    newState.manager.participants[secondParticipantId].selfStatus = ParticipantSelfStatus.Idle;
+
+    newState.manager.participants[participantId].callStatus = undefined;
+    newState.manager.participants[secondParticipantId].callStatus = ParticipantCallStatus.RemoteRejected;
+
+    newState.manager.participants[participantId].secondParticipantId = '';
+    newState.manager.participants[secondParticipantId].secondParticipantId = '';
+
+    this.setState(newState);
+  };
+
+  send = (participantId: string, message: string): void => {
+    const newState = { ...this.state };
+    const { secondParticipantId } = this.state.manager.participants[participantId];
+
+    newState.manager.participants[secondParticipantId].incomingMessage = message;
+
+    this.setState(newState);
+  }
+
+  endCall = (participantId: string): void => {
+    const newState = { ...this.state };
+    const { secondParticipantId } = this.state.manager.participants[participantId];
+
+    newState.manager.participants[participantId].selfStatus = ParticipantSelfStatus.Idle;
+    newState.manager.participants[secondParticipantId].selfStatus = ParticipantSelfStatus.Idle;
+
+    newState.manager.participants[participantId].callStatus = undefined;
+    newState.manager.participants[secondParticipantId].callStatus = undefined;
+
+    newState.manager.participants[participantId].secondParticipantId = '';
+    newState.manager.participants[secondParticipantId].secondParticipantId = '';
+
+    newState.manager.participants[secondParticipantId].incomingMessage = undefined;
+    newState.manager.participants[secondParticipantId].incomingMessage = undefined;
+
+    this.setState(newState);
+  };
 
   render() {
     const manager = this.state.manager;
@@ -58,5 +180,7 @@ export class MainPage extends React.Component {
 }
 
 function getRandomId(): string {
-  return Math.floor(Math.random() * 1000).toString();
+  return Math.floor(Math.random() * 1000)
+    .toString()
+    .padStart(3, '0');
 }
--- src/components/Participant/Participant.tsx
+++ src/components/Participant/Participant.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { ConnectionManager } from '../MainPage';
-import { ParticipantSelfStatus } from '../shared.model';
+import { ParticipantSelfStatus, ParticipantCallStatus } from '../shared.model';
+import './Participant.css';
 
 interface IProps {
   id: string;
@@ -16,7 +17,7 @@ class Participant extends React.Component<IProps> {
   state = {
     targetParticipant: '',
     hasIncomingCall: true,
-    status: ParticipantSelfStatus.Idle,
+    message: '',
   };
 
   handleTargetParticipantChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -29,6 +30,10 @@ class Participant extends React.Component<IProps> {
     }
   };
 
+  handleMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ message: event.target.value });
+  };
+
   render() {
     const id = this.props.id;
     const { targetParticipant } = this.state;
@@ -36,10 +41,19 @@ class Participant extends React.Component<IProps> {
     return (
       <ConnectionManager>
         {(manager) => {
+          const self = manager.participants[id];
+          const { callStatus, selfStatus } = self;
+          const isIdle = selfStatus === ParticipantSelfStatus.Idle;
+          const isRinning = selfStatus === ParticipantSelfStatus.Ringing;
+          const isTalking = selfStatus === ParticipantSelfStatus.Talking;
+          const isSelfId = targetParticipant === id;
+          const hasFailed = callStatus && callStatus !== ParticipantCallStatus.RemoteRinging;
+          const showCallStatus = (callStatus && !hasFailed) || (hasFailed && isIdle);
+          const hasIncomingMessage = typeof self.incomingMessage !== 'undefined';
           return (
             <div>
               <h1>Participant {id}</h1>
-              <h2>Status: {this.state.status}</h2>
+              <h2>Status: {selfStatus}</h2>
               <input
                 type="text"
                 maxLength={3}
@@ -47,13 +61,50 @@ class Participant extends React.Component<IProps> {
                 value={targetParticipant}
                 onChange={this.handleTargetParticipantChange}
               />
-              <button>Call</button>
-              {this.state.hasIncomingCall && (
-                <div>
-                  <button style={{ backgroundColor: 'red', color: 'white' }}>Decline</button>
-                  <button style={{ backgroundColor: 'green', color: 'white' }}>Answer</button>
-                </div>
+              {isTalking ? (
+                <button className="reject-button" onClick={() => manager.endCall(id)}>
+                  End call
+                </button>
+              ) : (
+                <button disabled={!isIdle || isSelfId} onClick={() => manager.makeCall(id, targetParticipant)}>
+                  Call
+                </button>
               )}
+              <div className="section-height-placeholder">
+                {showCallStatus && <span>Call Status: {callStatus}</span>}
+              </div>
+
+              <div className="section-height-placeholder">
+                {isTalking && (
+                  <>
+                    <div className="section-height-placeholder">
+                      {hasIncomingMessage && <div>Received message: {self.incomingMessage}</div>}
+                    </div>
+                    <input
+                      type="text"
+                      placeholder="Write your message here"
+                      value={this.state.message}
+                      onChange={this.handleMessageChange}
+                    />
+                    <button disabled={!this.state.message} onClick={() => manager.send(id, this.state.message)}>
+                      Send
+                    </button>
+                  </>
+                )}
+              </div>
+
+              <div>
+                {isRinning && (
+                  <>
+                    <button className="accept-button" onClick={() => manager.acceptCall(id)}>
+                      Answer
+                    </button>
+                    <button className="reject-button" onClick={() => manager.rejectCall(id)}>
+                      Decline
+                    </button>
+                  </>
+                )}
+              </div>
             </div>
           );
         }}
